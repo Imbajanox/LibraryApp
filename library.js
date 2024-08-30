00d1222660454dd039f379ec882cae0da1f3b630
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-30 10:12:10 +0200

    Create cards

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
@@ -14,6 +16,42 @@ function Book(title, author, pages, isRead){
     };
 }
 
-const book1 = new Book("The Hobbit", "J.R.R Tolien", 295, false);
+function displayLibrary(){
+    const libraryDiv = document.getElementById("library");
+    libraryDiv.innerHTML = '';
 
-console.log(book1.info());
+    myLibrary.forEach((book) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const title = document.createElement("h2");
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        const author = document.createElement("p");
+        author.textContent = `Author: ${book.author}`;
+        card.appendChild(author);
+
+        const pages = document.createElement("p");
+        pages.textContent = `Pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        const readStatus = document.createElement("p");
+        readStatus.textContent = book.isRead ? 'Read' : 'Not read';
+        readStatus.classList.add('read-status');
+        if(!book.isRead) {
+            readStatus.classList.add('unread');
+        }
+        card.appendChild(readStatus);
+
+
+        libraryDiv.appendChild(card);
+    });
+}
+
+
+myLibrary.push(new Book("The Hobbit", "J.R.R Tolien", 295, false));
+myLibrary.push(new Book("1984", "George Orwell", 328, false));
+myLibrary.push(new Book("Die Verwandlung", "Franz Kafka", 201, true));
+
+window.onload = displayLibrary;
